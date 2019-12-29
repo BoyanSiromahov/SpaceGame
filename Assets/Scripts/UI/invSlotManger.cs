c7ab60b48fca3284b3650023bd624f947d1bea01
Boyan Siromahov <boyan.siromahov@hotmail.com>
2019-12-28 22:30:54 -0500

    added tracking to inventory items

--- Assets/Scripts/UI/invSlotManger.cs
+++ Assets/Scripts/UI/invSlotManger.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class invSlotManger : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class invSlotManger : MonoBehaviour
     private Text nameTxt;
     private RawImage itemImage;
     public int numItems = -1;
-    private ArrayList items;
+    private ArrayList items = new ArrayList();
     public GameObject slotManager;
     public GameObject slotPrefab;
 
@@ -32,20 +33,35 @@ public class invSlotManger : MonoBehaviour
     public void addItem(GameObject nitem)
     {
         itemProperties itemProperties = nitem.GetComponentInChildren<itemProperties>();
-        GameObject slot = (GameObject)Instantiate(slotPrefab, slotManager.transform);
+        if (items.Contains(nitem))
+        {
+            foreach(Transform child in slotManager.GetComponent<Transform>())
+            {
+                if (child.transform.GetChild(3).GetComponent<Text>().text == itemProperties.itemName)
+                {
+                    var value = child.transform.GetChild(0).GetComponent<Text>().text;
+                    child.transform.GetChild(0).GetComponent<Text>().text = (Int32.Parse(value) + 1).ToString();
+                }
+            }
+        }
+        else
+        {
+            items.Add(nitem);
+            GameObject slot = (GameObject)Instantiate(slotPrefab, slotManager.transform);
 
-        //quantity text
-        slot.transform.GetChild(0).GetComponent<Text>().text = "TODO";
-        //description text
-        slot.transform.GetChild(1).GetComponent<Text>().text = itemProperties.description;
-        //value text
-        slot.transform.GetChild(2).GetComponent<Text>().text = itemProperties.value.ToString();
-        //name text
-        slot.transform.GetChild(3).GetComponent<Text>().text = itemProperties.itemName;
-        //raw image
-        slot.transform.GetChild(4).GetComponent<RawImage>().texture = Resources.Load(itemProperties.path) as Texture2D;
-        Vector2 smSize = slotManager.GetComponent<RectTransform>().sizeDelta;
-        Vector2 slotSize = slot.GetComponent<RectTransform>().sizeDelta;
-        slotManager.GetComponent<RectTransform>().sizeDelta = new Vector2(smSize.x, smSize.y + slotSize.y + 5);
+            //quantity text
+            slot.transform.GetChild(0).GetComponent<Text>().text = "1";
+            //description text
+            slot.transform.GetChild(1).GetComponent<Text>().text = itemProperties.description;
+            //value text
+            slot.transform.GetChild(2).GetComponent<Text>().text = itemProperties.value.ToString();
+            //name text
+            slot.transform.GetChild(3).GetComponent<Text>().text = itemProperties.itemName;
+            //raw image
+            slot.transform.GetChild(4).GetComponent<RawImage>().texture = Resources.Load(itemProperties.path) as Texture2D;
+            Vector2 smSize = slotManager.GetComponent<RectTransform>().sizeDelta;
+            Vector2 slotSize = slot.GetComponent<RectTransform>().sizeDelta;
+            slotManager.GetComponent<RectTransform>().sizeDelta = new Vector2(smSize.x, smSize.y + slotSize.y + 5);
+        }
     }
 }
